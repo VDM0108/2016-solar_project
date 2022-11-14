1bc2ebd2d51f28a47f2b742778fe3e4d8725a980
Вадим Лопаткин <vadimlopatkin@MacBook-Pro-Vadim-2.local>
2022-11-14 21:12:23 +0300

    1

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,12 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        x = abs(body.x - obj.x)
+        y = abs(body.y - obj.y)
+        sin = y/r
+        cos = x/r
+        body.Fx += (gravitational_constant)*body.m*obj.m*cos/(r**2)  # FIXME: нужно вывести формулу...
+        body.Fy += (gravitational_constant)*body.m*obj.m*sin/(r**2)  # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):
@@ -32,17 +36,19 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += body.Vx*dt  # FIXME: не понимаю как менять...
     body.Vx += ax*dt
     # FIXME: not done recalculation of y coordinate!
-
+    ay = body.Fy/body.m
+    body.y += body.Vy*dt  # FIXME: не понимаю как менять...
+    body.Vy += ay*dt
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
 
     Параметры:
 
-    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+    **space_objects** — список объектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
     """
 
